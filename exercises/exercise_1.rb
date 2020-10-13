a1e6c1fe476f45f99d49d462f14891fa7cb93deb
Vinay Baswa <vinaybaswa@gmail.com>
2020-10-13 02:35:52 +0000

    initial commit

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,10 @@
 require_relative '../setup'
+require_relative '../lib/store'
 
 puts "Exercise 1"
+store = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+
+puts Store_count
 puts "----------"
 
 # Your code goes below here ...
